2272842fa562bce5c26203e6d8e58d4bbc005886
Amarjeet Kumar <amarjeetkr23034@gmail.com>
2022-10-02 00:41:44 +0530

    problem solved and index updated
    n+nn+nnn solved in c and index updated

--- C Solutions/n+nn+nnn.c
+++ C Solutions/n+nn+nnn.c
@@ -2,9 +2,11 @@
 
 int main()
 {
-    int n;
+    int n,b,c;
     printf("enter an integer :: ");
     scanf("%d",&n);
-    printf("%d",n+n*n+n*n*n);
+    b = n+n*10;
+    c = n+n*10+n*100;
+    printf("%d",n+b+c);
     return 0;
 }
